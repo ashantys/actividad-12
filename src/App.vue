9f710fd7fbb30a12d56890d83ecb53cff1ecc82c
Ashanty Sierra <ashanty.sierram@gmail.com>
2024-04-16 16:49:16 -0600

    feat(jokes)se agregaron las bromas con estilos

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,28 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+<script>
+import ChuckJokes from './components/ChuckJokes.vue';
+
+export default {
+  components: {
+    ChuckJokes,
+  },
+  data() {
+    return {
+      chuckJokes: [
+        { value: "Chuck Norris can skydive into outer space."  },
+        { value: "The chief export of Chuck Norris is pain."  },
+        { value: "Chuck Norris doesn't read books. He stares them down until he gets the information he wants." },
+        { value: "Time waits for no man. Unless that man is Chuck Norris." },
+        { value: "If you spell Chuck Norris in Scrabble, you win. Forever." },
+        
+      ],
+    };
+  },
+};
 </script>
 
+
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <div id="app">
+    <ChuckJokes :chuckJokes="chuckJokes" />
+  </div>
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
---
+++ src/components/ChuckJokes.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="chuck-jokes">
+    <ul>
+        <li v-for="(joke, index) in chuckJokes" :key="index">{{ joke.value }}</li>
+    </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'ChuckJokes',
+    props: {
+      chuckJokes: Array, 
+    },
+};
+</script>
+
+<style scoped>
+.chuck-jokes {
+  background-color: #f9f9f9;
+  border: 1px solid #ddd;
+  padding: 10px;
+  margin: 20px 0;
+  border-radius: 5px;
+}
+
+.chuck-jokes ul {
+  list-style: inside;
+  padding: 0;
+}
+
+.chuck-jokes li {
+  margin-bottom: 10px;
+  font-size: 16px;
+  color: #333;
+}
+</style>
