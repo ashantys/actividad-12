43686559eff53bd7fb54e787d3ec9b22ae3392a1
Ashanty Sierra <ashanty.sierram@gmail.com>
2024-04-16 17:17:20 -0600

    feat(ChuckCard)se hizo el template y el srcipt de ChuckCard

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
-# actividad12
+# Actividad 12
 
 Se proporciona un arreglo llamado chuck con varias frases relacionadas con Chuck Norris.
 El objetivo es crear una aplicación Vue que muestre estas frases en una lista.
 La aplicación debe tener una hoja de estilo para que se vea agradable.
+
+# Tarea 10
---
+++ src/components/ChuckCard.vue
@@ -0,0 +1,22 @@
+<script>
+export default {
+  name: 'ChuckCard',
+  props: {
+    iconUrl: String, // Define el prop 'iconUrl' como una cadena
+    jokeValue: String, // Define el prop 'jokeValue' como una cadena
+  },
+};
+</script>
+
+<template>
+    <div class="chuck-card">
+    <div class="card">
+        <img :src="iconUrl" class="card-img-top" alt="Chuck Norris">
+        <div class="card-body">
+        <p class="card-text">{{ jokeValue }}</p>
+        </div>
+    </div>
+    </div>
+</template>
+
+
